ad6bca31ba42dbb894c4e46cc727657e6e1c10c1
49fp <elias.demmelbauer@kauz.ch>
2026-03-24 20:59:13 +0100

    Unittesting erste woche

--- fraction_test.ts
+++ fraction_test.ts
@@ -1,4 +1,4 @@
-import { assertAlmostEquals, assertEquals } from "@std/assert";
+import { assertAlmostEquals, assertEquals, assertThrows } from "@std/assert";
 import { Fraction } from "./fraction.ts";
 
 Deno.test("fraction of 1/1 is 1.0", () => {
@@ -33,4 +33,38 @@ Deno.test("1/3 + 2/6 = 2/3 is roughly 0.67", () => {
 
   // Assert
   assertAlmostEquals(left.toFloat(0.01), 0.67);
+});
+
+Deno.test("1/2 * 2/1 is 1.0", () => {
+  // Arrange
+  const left = new Fraction(1, 2);
+  const right = new Fraction(2, 1);
+
+  // Act
+ left.multiply(right);
+
+  // Assert
+  assertEquals(left.toFloat(0.01), 1.0);
+});
+
+Deno.test("3/4 - 1/4 is 0.5", () => {
+  // Arrange
+  const left = new Fraction(3, 4);
+  const right = new Fraction(1, 4);
+
+  // Act
+  left.subtract(right);
+
+  // Assert
+  assertEquals(left.toFloat(0.01), 0.5);
+});
+
+
+Deno.test("denomintor = 0 throws error", () => {
+  // Arrange
+
+  // Act
+
+  // Assert
+  assertThrows(() => { new Fraction(3, 0) });
 });
--- geometry_test.ts
+++ geometry_test.ts
@@ -11,3 +11,31 @@ Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
   // Then
   assertAlmostEquals(actual, 31.416, 0.01);
 });
+
+
+
+
+Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
+  // Given
+  const circle = new Circle(new Point2D(0, 0), 5);
+
+  // When
+  const actual = circle.circumference();
+
+  // Then
+  assertAlmostEquals(actual, 31.416, 0.01);
+})
+
+// kurze note: ich hab mir denn bisher bestehenden code erklären lassen mit Ki.
+// Da ich bei der mathematik nicht mehr ganz durch schaute
+
+Deno.test("diameter of circle with radius 3 is 6", () => {
+  // Given
+  const circle = new Circle(new Point2D(0, 0), 3);
+
+  // When
+  const actual = circle.diameter();
+
+  // Then
+  assertAlmostEquals(actual, 6);
+});
